9d129632f4e4d8fc2d9d0478b8178d5208d2537b
Jon Cram <webignition@gmail.com>
2020-08-04 14:42:05 +0100

    Fix CS

--- tests/Unit/Services/ListenSocketFactoryTest.php
+++ tests/Unit/Services/ListenSocketFactoryTest.php
@@ -4,17 +4,10 @@ declare(strict_types=1);
 
 namespace webignition\DockerTcpCliProxy\Tests\Unit\Services;
 
-use Mockery;
 use PHPUnit\Framework\TestCase;
 use Socket\Raw\Factory;
-use webignition\DockerTcpCliProxy\Model\CommunicationSocket;
 use webignition\DockerTcpCliProxy\Model\ListenSocket;
-use webignition\DockerTcpCliProxy\Services\ClientHandler;
-use webignition\DockerTcpCliProxy\Services\ClientHandlerFactory;
-use webignition\DockerTcpCliProxy\Services\CommandReader;
-use webignition\DockerTcpCliProxy\Services\CommunicationSocketFactory;
 use webignition\DockerTcpCliProxy\Services\ListenSocketFactory;
-use webignition\DockerTcpCliProxy\Services\ResponseWriter;
 
 class ListenSocketFactoryTest extends TestCase
 {
